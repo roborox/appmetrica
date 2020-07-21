992ffc644cf06c73fc49bad801d7f0c658da2818
Mikhaylov Anton <anion155@gmail.com>
2020-07-21 21:47:40 +0500

    fix: ios build

--- ios/Appmetrica-Bridging-Header.h
+++ ios/Appmetrica-Bridging-Header.h
@@ -1,1 +1,2 @@
 #import <React/RCTBridgeModule.h>
+#import <YandexMobileMetrica/YandexMobileMetrica.h>
--- ios/Appmetrica.swift
+++ ios/Appmetrica.swift
@@ -2,59 +2,59 @@
 class Appmetrica: NSObject {
     @objc(reportUserProfile:)
     func reportUserProfile(profile:NSDictionary!) -> Void {
-        let attrsArray:NSMutableArray! = NSMutableArray.array()
+        var attrsArray:[YMMUserProfileUpdate] = []
 
-        for key:String! in profile {
+        for (keyAny, _) in profile {
+            let key = keyAny as! String
             if key.isEqual("name") {
-                if attributes[key] == nil {
-                    attrsArray.addObject(YMMProfileAttribute.name().withValueReset())
+                if profile[key] == nil {
+                    attrsArray.append(YMMProfileAttribute.name().withValueReset())
                 } else {
-                    attrsArray.addObject(YMMProfileAttribute.name().withValue(attributes[key]))
+                    attrsArray.append(YMMProfileAttribute.name().withValue(profile[key] as? String))
                 }
-            // } else if key.isEqual("gender") {
+                // } else if key.isEqual("gender") {
             } else if key.isEqual("age") {
-                if attributes[key] == nil {
-                    attrsArray.addObject(YMMProfileAttribute.birthDate().withValueReset())
+                if profile[key] == nil {
+                    attrsArray.append(YMMProfileAttribute.birthDate().withValueReset())
                 } else {
-                    attrsArray.addObject(YMMProfileAttribute.birthDate().withAge(attributes[key].intValue()))
+                    attrsArray.append(YMMProfileAttribute.birthDate().withAge(profile[key] as! UInt))
                 }
             } else if key.isEqual("birthDate") {
-                if attributes[key] == nil {
-                    attrsArray.addObject(YMMProfileAttribute.birthDate().withValueReset())
+                if profile[key] == nil {
+                    attrsArray.append(YMMProfileAttribute.birthDate().withValueReset())
                 } else {
                     // number of milliseconds since Unix epoch
-                    let date:NSDate! = attributes[key].date()
-                    let gregorian:NSCalendar! = NSCalendar(calendarIdentifier:NSCalendarIdentifierGregorian)
-                    let dateComponents:NSDateComponents! = gregorian.components((NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay), fromDate:date)
-                    attrsArray.addObject(YMMProfileAttribute.birthDate().withDateComponents(dateComponents))
+                    let date = Date.init(timeIntervalSince1970: profile[key] as! Double)
+                    let gregorian = NSCalendar(calendarIdentifier:NSCalendar.Identifier.gregorian)!
+                    let dateComponents = gregorian.components([.day, .month, .year], from:date)
+                    attrsArray.append(YMMProfileAttribute.birthDate().withDate(dateComponents: dateComponents))
                 }
             } else if key.isEqual("notificationsEnabled") {
-                if attributes[key] == nil {
-                    attrsArray.addObject(YMMProfileAttribute.notificationsEnabled().withValueReset())
+                if profile[key] == nil {
+                    attrsArray.append(YMMProfileAttribute.notificationsEnabled().withValueReset())
                 } else {
-                    attrsArray.addObject(YMMProfileAttribute.notificationsEnabled().withValue(attributes[key].boolValue()))
+                    attrsArray.append(YMMProfileAttribute.notificationsEnabled().withValue(profile[key] as! Bool))
                 }
             } else {
                 // TODO: come up with a syntax solution to reset custom attributes. `null` will break type checking here
-                if attributes[key].isEqual(true) || attributes[key].isEqual(false) {
-                    attrsArray.addObject(YMMProfileAttribute.customBool(key).withValue(attributes[key].boolValue()))
-                } else if (attributes[key] is NSNumber) {
-                    attrsArray.addObject(YMMProfileAttribute.customNumber(key).withValue(attributes[key].doubleValue()))
+                if profile[key] is Bool {
+                    attrsArray.append(YMMProfileAttribute.customBool(key).withValue(profile[key] as! Bool))
+                } else if (profile[key] is NSNumber) {
+                    attrsArray.append(YMMProfileAttribute.customNumber(key).withValue(profile[key] as! Double))
                     // [NSNumber numberWithInt:[attributes[key] intValue]]
-                } else if (attributes[key] is NSString) {
-                    if attributes[key].hasPrefix("+") || attributes[key].hasPrefix("-") {
-                        attrsArray.addObject(YMMProfileAttribute.customCounter(key).withDelta(attributes[key].doubleValue()))
+                } else if (profile[key] is NSString) {
+                    let value = profile[key] as! NSString
+                    if value.hasPrefix("+") || value.hasPrefix("-") {
+                        attrsArray.append(YMMProfileAttribute.customCounter(key).withDelta(value.doubleValue))
                     } else {
-                        attrsArray.addObject(YMMProfileAttribute.customString(key).withValue(attributes[key]))
+                        attrsArray.append(YMMProfileAttribute.customString(key).withValue(value as String))
                     }
                 }
             }
          }
 
-        let userProfile:YMMMutableUserProfile! = YMMMutableUserProfile()
-        userProfile.applyFromArray(attrsArray)
-        YMMYandexMetrica.reportUserProfile(userProfile.copy(), onFailure:{ (error:NSError!) in
-            NSLog("Error: %@", error)
-        })
+        let userProfile:YMMMutableUserProfile = YMMMutableUserProfile()
+        userProfile.apply(from: attrsArray)
+        YMMYandexMetrica.report(userProfile, onFailure:{ (error:Error) in })
     }
 }
